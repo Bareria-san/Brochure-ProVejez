ebcc1177f92fd4ee6e80ee379a3039500a45b5f3
Snoppy0817 <Snoppy0817@gmail.com>
2021-07-15 00:04:22 -0500

    Terminación de página inicio, creación de página proyecto

---
+++ main.js
@@ -0,0 +1,26 @@
+document.querySelector('.btn-menu').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('mostrar')
+})
+
+ScrollReveal().reveal('.case')
+ScrollReveal().reveal('.cards', { delay: 400 });
+
+var card2 = document.querySelector(".card2");
+var playing = false;
+
+card2.addEventListener('click',function() {
+  if(playing)
+    return;
+  
+  playing = true;
+  anime({
+    targets: card2,
+    scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+    easing: 'easeInOutSine',
+    duration: 400,
+    complete: function(anime){
+       playing = false;
+    }
+  });
+});
